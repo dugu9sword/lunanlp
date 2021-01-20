eb833986a6a00e3f41c35a6adbb5b7083ff2c014
zhouyi <dugu9sword@163.com>
2021-01-20 11:52:30 +0800

    backup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="luna",
-    version="inf",
+    name="lunanlp",
+    version="1.0",
     keywords=["luna", ],
     description="eds sdk",
     long_description="My tool for research",
     license="WTFPL Licence",
 
-    url="https://github.com/dugu9sword/luna",
+    url="https://github.com/dugu9sword/lunanlp",
     author="dugu9sword",
     author_email="dugu9sword@163.com",
 
